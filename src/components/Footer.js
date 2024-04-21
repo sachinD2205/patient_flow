8fc2e4c96f9e3101b63534bd902e9a7d81b88caa
Sachin Durge <sachindurge2205@gmail.com>
2024-04-21 09:58:05 +0530

    ADD - Layout Changes

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,55 @@
-import React from "react";
+import * as React from "react";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import Menu from "@mui/material/Menu";
+import MenuIcon from "@mui/icons-material/Menu";
+import Container from "@mui/material/Container";
+import Avatar from "@mui/material/Avatar";
+import Button from "@mui/material/Button";
+import Tooltip from "@mui/material/Tooltip";
+import MenuItem from "@mui/material/MenuItem";
+import EscalatorWarningRoundedIcon from "@mui/icons-material/EscalatorWarningRounded";
 
-const Footer = () => {
+const pages = ["Products", "Pricing", "Blog"];
+const settings = ["Profile", "Account", "Dashboard", "Logout"];
+
+// Footer
+function Footer() {
+  const [anchorElNav, setAnchorElNav] = React.useState(null);
+  const [anchorElUser, setAnchorElUser] = React.useState(null);
+
+  const handleOpenNavMenu = (event) => {
+    setAnchorElNav(event.currentTarget);
+  };
+  const handleOpenUserMenu = (event) => {
+    setAnchorElUser(event.currentTarget);
+  };
+
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
+  };
+
+  const handleCloseUserMenu = () => {
+    setAnchorElUser(null);
+  };
+
   return (
-    <div>
-      <p>&copy; 2024 My Application</p>
-    </div>
+    <AppBar sx={{ position: { xs: "sticky", md: "relative" } }}>
+      <Container
+        maxWidth="xl"
+        sx={{
+          backgroundColor: "lightgrey",
+          color: "black",
+          display: "flex",
+          justifyContent: "center",
+        }}
+      >
+        <p>&copy; 2024 PHN Technology</p>
+      </Container>
+    </AppBar>
   );
-};
-
+}
 export default Footer;
