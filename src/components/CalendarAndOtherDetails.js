cafdf4fc04025f27588a7fd6cdc024ceaa5a0f08
Sachin Durge <sachindurge2205@gmail.com>
2024-04-22 04:58:01 +0530

    ADD - New

--- src/components/CalendarAndOtherDetails.js
+++ src/components/CalendarAndOtherDetails.js
@@ -1,7 +1,20 @@
 import React from "react";
+import CalendarForAppointmentView from "./CalendarForAppointmentView";
+import PatientDetailsHome from "./PatientDetailsHome";
+import styles from "../styles/CalendarAndOtherDetails.module.css";
+import { Paper } from "@mui/material";
 
 const CalendarAndOtherDetails = () => {
-  return <div>CalendarAndOtherDetails</div>;
+  return (
+    <div className={styles.mainDiv}>
+      <div>
+        <CalendarForAppointmentView />
+      </div>
+      <div>
+        <PatientDetailsHome />
+      </div>
+    </div>
+  );
 };
 
 export default CalendarAndOtherDetails;
--- src/components/CalendarForAppointmentView.js
+++ src/components/CalendarForAppointmentView.js
@@ -1,7 +1,18 @@
-import React from "react";
+import * as React from "react";
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import { DateCalendar } from "@mui/x-date-pickers/DateCalendar";
+import styles from "../styles/CalendarForAppointmentView.module.css";
+import { Paper } from "@mui/material";
 
 const CalendarForAppointmentView = () => {
-  return <div>CalendarForAppointmentView</div>;
+  return (
+    <Paper square elevation={5} className={styles.Paper}>
+      <LocalizationProvider dateAdapter={AdapterDayjs}>
+        <DateCalendar />
+      </LocalizationProvider>
+    </Paper>
+  );
 };
 
 export default CalendarForAppointmentView;
